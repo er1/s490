3e1a558a42ec43ecb19eee3432385991b6f22e2a
C-o-r-E <can.of.tuna@gmail.com>
2011-06-09 11:05:44 -0400

    Modified hexdump unit test

--- sim/unit_tests/hexdump.c
+++ sim/unit_tests/hexdump.c
@@ -1,12 +1,26 @@
 #include <stdio.h> 
+#include <string.h>
+
 
 #include "../util.h"
 
 int main(){
 
-  char message[] = "Have you ever herped so hard you derped?";
+  char msg[] = "Have you ever herped so hard you derped?";
+  struct derp{
+    int a;
+    char message[48];
+    short b;
+    char c;
+  };
 
-  hexDump(message, sizeof message); 
+  struct derp herp;
+  herp.a = 0x123456;
+  strncpy(herp.message, msg, 48);
+  herp.b = 1;
+  herp.c = '!';
+
+  hexDump(&herp, sizeof(struct derp)); 
 
   return 0; 
 }
