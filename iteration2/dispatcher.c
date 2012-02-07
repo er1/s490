0d3462f62f3c283fe59a4070a99de41a971467c2
C-o-r-E <can.of.tuna@gmail.com>
2012-02-07 14:46:15 -0500

    fixed Makefile for proper systems

--- iteration2/dispatcher.c
+++ iteration2/dispatcher.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include "common.h"
+#include "jobdef.h"
 
 // allow a job to run
 	// spawn a new process
@@ -13,7 +14,40 @@
 
 // recv commands through some means
 
+//need a list of jobs
+job_instance * jList;
+int jList_cap = 0;
+int jList_num = 0;
+
+void addJob(char * n, int id)
+{
+  jList[jList_num] = 
+}
+
 int main() {
+  
+  //start with an empty list of size 8
+  jList_cap = 8;
+  jList = ;
+  
+
+  //read some config that tells us what to run.
+  char jName[64];
+  int id = 0;
+  FILE * fd = fopen("jobs.txt", "r");
+  if(!fd)
+    {
+      printf("Failed to open jobs.txt\n");
+    }
+
+  while(1)
+    {
+      int t = fscanf(fd, "%s %d\n", jName, &id);
+      if(t == EOF)
+	break;
+      
+      printf("Found Job [%s] with ID=%d\n", jName, id);
+    }
 
 	log("Starting up...\n");
 
