7fda7f29ef38fbc3c2dd9eefc63320a04962ccf0
Mathieu Dumais-Savard <matdumsa@gmail.com>
2012-03-17 18:57:32 -0400

    Added code to detect when a connection is closed improperly and reset its KI owner to -1 to another KS can join in and continue

--- Blackboard/Blackboard.h
+++ Blackboard/Blackboard.h
@@ -35,7 +35,8 @@ private:
 
     void prepareSelect();
     void handlePacket(int, const Packet&);
-
+    void cleanClosedConnection();
+    
     // instancing
     static Blackboard* instance;
     Blackboard();
