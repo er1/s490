bee594344463423cedfe4b3489cad658ebd1b958
C-o-r-E <can.of.tuna@gmail.com>
2012-03-17 06:00:03 -0400

    added some comments

--- Blackboard/monitorKSCS.cpp
+++ Blackboard/monitorKSCS.cpp
@@ -15,6 +15,38 @@ int main(int argc, char** argv) {
     BlackboardConnection& blackboard = *(new BlackboardConnection);
     blackboard.connectBB(BB_SOCK_PATH);
 
+
+	/*
+	  I assume that this is meant to be the test case for
+	  both the KS and CS interface. The way I still see the 
+	  ouside facing api to be is the following:
+
+	  	knowledgeSource * ks = new knowledgeSource(42);
+		controlShell * cs =  new controlShell();
+
+		ks->init();
+		cs->init();
+		
+		ks->reg();
+		
+		uint8_t * d =  new uint8_t[4];
+		
+		d[0] = 'd';
+		d[1] = 'e';
+		d[2] = 'r';
+		d[3] = 'p';
+		cs->getLast(tag, &d);
+		
+		deque<dataPoint> v = cs->getLast(tag, 10);
+
+
+		ks->update(4, d);
+		
+		ks->close();
+
+	 */
+
+
     /*
     while (true) {
         Packet p;
