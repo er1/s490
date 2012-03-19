5b1ab445661c246003be75eecdfec31e9969dd54
C-o-r-E <can.of.tuna@gmail.com>
2012-03-19 18:36:27 -0400

    modified testKS to use minunit

--- Blackboard/testKS.cpp
+++ Blackboard/testKS.cpp
@@ -1,49 +1,63 @@
 #include "KnowledgeSource.h"
 #include "BlackboardConnection.h"
 #include "unistd.h"
+#include "stdio.h"
+#include "minunit.h"
+
+int tests_run = 0;
+
+int tag = 42;
+int delayBetweenUpdateInMs = 100;
+
+KnowledgeSource * myKS;
+
+DataPoint p;
+
+static const char * test_connect(){
+	bool connected = myKS->connectKS();
+	mu_assert("Could not connect, BB did not allow me to register for tag", connected == true);
+	return 0;
+}
+
+static const char * test_update(){
+	p.resize(4);
+	p[0] = 'd';
+	p[1] = 'e';
+	p[2] = 'r';
+	p[3] = 'p';
+	bool updated = myKS->update(p);
+	mu_assert("update failed", updated == true);
+	return 0;
+}
+
+static const char * all_tests(){
+	mu_run_test(test_connect);
+	mu_run_test(test_update);
+	return 0;
+}
 
 int main(int argc, char** argv) {
 
-    int tag = 42;
     if (argc > 1){
         tag = atoi(argv[1]);
     }
-    int delayBetweenUpdateInMs = 100;
+
     if (argc > 2){
         sscanf(argv[2], "%d", &delayBetweenUpdateInMs);
     }
-    KnowledgeSource myKS(tag);
+	myKS = new KnowledgeSource(tag);
 
-    if (myKS.connectKS()) {
-        log("connected\n");
-    } else {
-        log("Could not connect, BB did not allow me to register for tag %d\n", tag);
-        return 1;
-    }
-
-    DataPoint p;
-    p.resize(4);
-
-    while (argc > 2) {
-
-        log("updating... ");
-        
-        p[0] = 'd';
-        p[1] = 'e';
-        p[2] = 'r';
-        p[3] = 'p';
-        if (myKS.update(p)) {
-            log("success\n");
-        } else {
-            log("failed\n");
-        }
-
-        // 100 ms * 1000 ns per ms;
-        usleep(delayBetweenUpdateInMs * 1000);
-    }
-
-    myKS.disconnectKS();
-
+	const char *result = all_tests();
+	if (result != 0) {
+		printf("%s\n", result);
+	}
+	else {
+		printf("ALL TESTS PASSED\n");
+	}
+	printf("Tests run: %d\n", tests_run);
+	
+    myKS->disconnectKS();
     return 0;
 }
-
+       // 100 ms * 1000 ns per ms;
+//        usleep(delayBetweenUpdateInMs * 1000);
