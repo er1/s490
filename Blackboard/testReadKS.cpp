7e1d73456e0f13f33059d7568ea38ac7be050a00
Eric Chan <er_cha@cs.concordia.ca>
2012-03-21 20:50:46 -0400

    removed cruft, copied an old example over

--- Blackboard/testReadKS.cpp
+++ Blackboard/testReadKS.cpp
@@ -1,4 +1,16 @@
-#include "testRead.h"
+#include <stdio.h>
+#include <semaphore.h>  /* Semaphore */
+#include <time.h>
+#include <math.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define PI (3.141592654)
+
+void run(sem_t * semaphore);
 
 #define DATAPATH ("data/testdata1.fifo")
 
