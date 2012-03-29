49c7f8558a76bd156e7e9c1567fe46e86ef3f0cb
Eric Chan <er_cha@cs.concordia.ca>
2012-03-29 13:40:32 -0400

    minor demo modification

--- Blackboard/timePR.cpp
+++ Blackboard/timePR.cpp
@@ -3,22 +3,32 @@
 #include <common/ControlShell.h>
 #include "time.h"
 
-KnowledgeSource myKS(BBTAG_DEMO10BOOL);
+KnowledgeSource myKS1(BBTAG_DEMO10BOOL);
+KnowledgeSource myKS2(BBTAG_DEMO10BOOL + 1);
+
 ControlShell myCS(BBTAG_SIMPLETIME);
 DataPoint dp;
 
+int last = 0;
+
 void update(bbtag, const DataPoint& p) {
     time_t t = p.unwrap<time_t > ();
-    if (t % 20 < 10)
-        dp.wrap(1);
-    else
-        dp.wrap(0);
-    myKS.update(dp);
+    int next = 0;
+    if (t % 20 < 10) {
+        next = 1;
+    }
+    if (next != last) {
+        dp.wrap(next);
+        myKS1.update(dp);
+        dp.wrap(1 - next);
+        myKS2.update(dp);
+    }
+    last = next;
 }
 
 int main() {
 
-    if ((!myCS.connectCS()) || (!myKS.connectKS())) {
+    if ((!myCS.connectCS()) || (!myKS1.connectKS()) || (!myKS2.connectKS())) {
         fprintf(stderr, "connection error!\n");
         exit(2);
     }
@@ -27,13 +37,15 @@ int main() {
 
     std::deque<BlackboardConnection*> bbconns;
     bbconns.push_back(&myCS);
-    bbconns.push_back(&myKS);
+    bbconns.push_back(&myKS1);
+    bbconns.push_back(&myKS2);
 
     while (true) {
         BlackboardConnection::multiWait(bbconns);
         myCS.processMsgQueue();
         myCS.checkForUpdates();
-        myKS.processMsgQueue();
+        myKS1.processMsgQueue();
+        myKS2.processMsgQueue();
     }
     return 0;
 }
